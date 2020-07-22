e45febf9cec75001ac512eee57cf01a248b53ec7
Ali Zubair <61495779+AliZ801@users.noreply.github.com>
2020-07-22 20:37:25 +0300

    Updates

---
+++ precache-manifest.dc850982cfcf71c1534735c2b9fe1760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798e935e7eb5f8584c818ffdfc6f37c5",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "5c718510b72f3aa0029c",
+    "url": "/monsters-roledex/static/css/main.a61ea2ec.chunk.css"
+  },
+  {
+    "revision": "639c49fbd6946a082aa3",
+    "url": "/monsters-roledex/static/js/2.eb0c56c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.eb0c56c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c718510b72f3aa0029c",
+    "url": "/monsters-roledex/static/js/main.1f2a7dea.chunk.js"
+  },
+  {
+    "revision": "8cc887446fafdb4fa067",
+    "url": "/monsters-roledex/static/js/runtime-main.ac70d4da.js"
+  }
+]);
